bee54b45d0a71ac9caf623685b14c8347fbdc623
Philip Bradbury <philipbradbury1@gmail.com>
2022-05-25 20:31:04 +0100

    updates

--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,5 @@ import store from './store';
 import VueGtag from "vue-gtag";
 
 createApp(App).use(VueGtag, {
-    config: { id: "UA-88383366-1" }
+    config: { id: "GTM-TJFHW3S" }
   }).use(store).use(router).mount('#app');
