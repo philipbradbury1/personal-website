64751b0456cfac991c1fa056fe0de78c4e70bab7
Philip Bradbury <philipbradbury1@gmail.com>
2022-06-10 21:49:40 +0100

    about fixes

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,26 +1,32 @@
 <template>
     <section id="about">
 
-        <div class="about__text">
+        <div>
             <h3>About Me</h3>
-
-            <p>Hello, my name is Philip and I'm a web developer. I've had an interest in technology and engineering from a very young age. I got into building static websites when I was about 13 years old when I started to backwards engineer other websites to see how they were built. </p>
-            <p>Today I work for a web agency/start-up based in Huddersfield where we develop a SaaS CRM application. We also build stunning and engaging websites to create an immersive experiance to increase user ingagement.    </p>
-            <p>Here is a list of some of the technologies I use:</p>
-            <ul>
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>JavaScript/TypeScript</li>
-                <li>Angular</li>
-                <li>Vue</li>
-                <li>WordPress</li>
-                <li>PHP</li>
-                <li>Docker</li>
-            </ul>
         </div>
 
-        <div class="about__img text-center">
-            <img src="@/assets/images/profile.jpg" alt="Image of Philip Bradbury">
+        <div class="about__container">
+            <div class="about__text">
+
+                <p>Hello, my name is Philip and I'm a web developer. I've had an interest in technology and engineering from a very young age. I got into building static websites when I was about 13 years old when I started to backwards engineer other websites to see how they were built. </p>
+                <p>Today I work for a web agency/start-up based in Huddersfield where we develop a SaaS CRM application. We also build stunning and engaging websites to create an immersive experiance to increase user ingagement.    </p>
+                <p>Here is a list of some of the technologies I use:</p>
+                <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript/TypeScript</li>
+                    <li>Angular</li>
+                    <li>Vue</li>
+                    <li>WordPress</li>
+                    <li>PHP</li>
+                    <li>Docker</li>
+                </ul>
+            
+            </div>
+
+            <div class="about__img text-center">
+                <img src="@/assets/images/profile.jpg" alt="Image of Philip Bradbury">
+            </div>
         </div>
     </section>
 </template>
@@ -37,19 +43,38 @@ export default defineComponent({
 
 <style lang="scss">
 
+@import "@/assets/scss/variables.scss";
+
 #about{
-    margin-top:80px;
-    justify-content: space-evenly;
-    flex-direction: row;
-    display: flex;
+    margin: 80px 70px 0px;
+
+    .about__container{
+        flex-direction: row;
+        display: flex;
+        justify-content: space-between;
+    }
 
     .about__text{
         vertical-align:top;
-        flex-basis: 40%;
+        flex-basis: 45%;
+
+        li{
+            position: relative;
+            padding-left:  20px;
+        }
+
+        li::before {
+            content: "\25CF";
+            position: absolute;
+            left: 0px;
+            color: $neon-purple;
+            font-size: 18px;
+            line-height: 22px;
+        }
     }
 
     .about__img{
-        flex-basis: 40%;
+        flex-basis: 45%;
             
 
         img{
@@ -84,6 +109,7 @@ export default defineComponent({
 
         .about__img{
             img{
+                margin-top: 50px;
                 padding: 0 20px;
 
             }
@@ -91,15 +117,17 @@ export default defineComponent({
 
     }
 }
-  
-
 
 
   @media screen and (max-width:768px) {
 
     #about{
 
-        flex-direction: column;
+        margin: 40px 0px 0px;
+
+        .about__container{
+            flex-direction: column;
+        }     
 
         .about__text,
         .about__img{
